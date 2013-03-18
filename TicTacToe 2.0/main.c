79b10d5193a44a31a347fa36d250fca7efb12a84
Developer <developer@Macs-Mac-mini.local>
2013-03-18 16:31:22 +0100

    Initial Commit

---
+++ TicTacToe 2.0/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TicTacToe 2.0
+//
+//  Created by Developer on 18.03.13.
+//  Copyright (c) 2013 Developer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
